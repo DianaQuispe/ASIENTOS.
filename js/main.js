6b16a46f766b609f34d08d4d0136df45236c64e4
MyDiana <danaalienmas@gmail.com>
2017-09-25 07:25:02 -0500

    act

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 'use strict';
 class List {
     constructor(userId, id, title, completed) {
-        this.userId = userId;
-        this.id = 1;
         this.title = title;
         this.completed = false;
-        this.array  = [];
-        
     }
-   
 
     add() {
         var newList = '<ul><li>';
         if($('#text').val() != ''){
-            $('#list').append( newList +'<i  class="fa fa-times-circle" aria-hidden="true"></i> </n></n>' + $('#text').val() +  '</li><i id="slow" class="fa fa-times-circle close" aria-hidden="true"></i></ul>');
+            $('#list').append( newList + $('#text').val());
             $('#list').attr('contentEditable','true');
-            $('#text').val('');            
+            $('#text').val('');   
+            
         }else alert('ponga lista');
         
     }
-    slow() {
-        $('li').fadeOut('slow');
+    done() {
+    
+    //    $('li').wrap("<del>");
+    (event.target).style.textDecoration =  ((event.target).style.textDecoration =="line-through") ? 'transparent' : "line-through" ;
+    
+
     }
     start() {
         $('#add').click(() => this.add());
-        $('#slow').click(() => this.slow());
-     
-
+        $('#list').click(() => this.done());
         
     }
  }
     $(document).ready(()=> {
         var list = new List();
         list.start();
+        $('#add').keypress(function(e) {
+            if(e.which == 13) {
+                alert('You pressed enter!');
+            }
+        });
     })
+    
